b16c0e687c6b09de0afeb18942dbdeff1f5d4d86
Duha-H <duha.h.153@gmail.com>
2019-09-08 10:05:25 -0400

    Added access of git repo url

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as cp from 'child_process';
 import { ProjectsProvider } from './viewComponents/projectsProvider';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -12,10 +15,19 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('soup');
 	});
 
-	const projects = new ProjectsProvider();
-	vscode.window.registerTreeDataProvider('github-projects', projects); // register a data provider to a view
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	const workspaceRoot = workspaceFolders
+		? workspaceFolders[0].uri.fsPath
+		: '';
+	const projectsProvider = new ProjectsProvider(workspaceRoot);
+	vscode.window.registerTreeDataProvider('github-projects', projectsProvider); // register a data provider to a view
 
 	context.subscriptions.push(disposable);
+	//const wsFolders = vscode.workspace.workspaceFolders;
+	//const rootPath = wsFolders ? wsFolders[0].uri.fsPath : '';
+	//const configPath = path.join(rootPath, '.git', 'config');
+	//let contents = fs.readFileSync(configPath, 'utf-8');
+	//console.log(contents);
 }
 
 export function deactivate() {}
--- src/viewComponents/projectsProvider.ts
+++ src/viewComponents/projectsProvider.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as cp from 'child_process';
 import {Project} from './projects';
 
 export class ProjectsProvider implements vscode.TreeDataProvider<Project> {
@@ -8,22 +7,78 @@ export class ProjectsProvider implements vscode.TreeDataProvider<Project> {
 	private _onDidChangeTreeData: vscode.EventEmitter<Project | undefined> = new vscode.EventEmitter<Project | undefined>();
 	readonly onDidChangeTreeData: vscode.Event<Project | undefined> = this._onDidChangeTreeData.event;
 
-	constructor(private workspaceRoot?: string) {
-	}
+	constructor(private workspaceRoot: string) {	}
 
-    /*
+	/*
 	refresh(): void {
 		this._onDidChangeTreeData.fire();
-    }
-    */
+	}
+	*/
 
 	getTreeItem(element: Project): vscode.TreeItem {
 		return element;
 	}
 
-	getChildren(element?: Project): vscode.ProviderResult<Project[]> {
-        let dep = new Project('moduleName', vscode.TreeItemCollapsibleState.Collapsed);
-		return [dep];
+	getChildren(element?: Project): Thenable<Project[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showErrorMessage('No git reposityory found: workspace is empty.');
+			console.log('Workspace empty');
+			return Promise.resolve([]);
+		}
+		
+		let gitUrl = this.getGitUrl();
+		if (gitUrl.length === 0) {
+			return Promise.resolve([]);			
+		} else {
+			let len = gitUrl.split('/').length;
+			let projectName = gitUrl.split('/')[len - 1];
+			let project = new Project(projectName, vscode.TreeItemCollapsibleState.Collapsed);
+			//console.log(projectName);
+			return Promise.resolve([project]);
+		}
+	}
+
+	getGitUrl(): string {
+		let url = '';
+		let gitConfig : string[] = [];
+
+		/*
+		cp.exec('git config --list', {
+			cwd: this.workspaceRoot
+		}, (err, content) => {
+			if (err) {
+				vscode.window.showErrorMessage('Error obtaining git repository configuration.');
+				console.log('No git url obtained: ', err);
+				return url;
+			}
+			//console.log(content);
+			gitConfig = content.split('\n');
+			gitConfig.forEach((entry) => {
+				if (entry.includes('remote.origin.url')) {
+					url = entry.split('=')[1];
+					//console.log(url);
+					return url;
+				}
+			});
+		});
+		*/
+		gitConfig = cp.execSync('git config --list', {
+				cwd: this.workspaceRoot
+			})
+			.toString()
+			.split('\n');
+		gitConfig.forEach((entry) => {
+			if (entry.includes('remote.origin.url')) {
+				url = entry.split('=')[1];
+				console.log('Obtained url: ', url);
+			}
+		});
+		if (url.length === 0) {
+			vscode.window.showErrorMessage('No url obtained.');
+			console.log('something\'s up');
+		}
+
+		return url;
 	}
 
 }
